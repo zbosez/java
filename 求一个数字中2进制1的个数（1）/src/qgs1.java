599f009e67a9d4c0a9559955403b3bd1baede67e
zbossz <430561907@qq.com>
2021-07-14 15:51:45 +0800

    方法1

---
+++ 求一个数字中2进制1的个数（1）/src/qgs1.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: zbossz
+ * Date: 2021-07-14
+ * Time: 15:50
+ */
+
+public class qgs1 {
+    public static void main(String[] args) {
+        int num =15;
+        int count1 =0;
+        while(num!=0){
+            if((num&1)==1){
+                num&=(num-1);
+                count1++;
+            }
+            num>>>=1;
+        }
+        System.out.println(count1);
+    }
+}
