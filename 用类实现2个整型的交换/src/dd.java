17f9fc107360f73853e2f3db67a328254c209e51
zbossz <430561907@qq.com>
2021-07-25 18:45:36 +0800

    1

---
+++ 用类实现2个整型的交换/src/dd.java
@@ -0,0 +1,20 @@
+class Func{
+    public int num;
+
+}
+public class dd {
+    public static void Swap(Func x,Func y){
+        int temp=x.num;
+        x.num=y.num;
+        y.num =temp;
+    }
+    public static void main(String[] args) {
+       Func func = new Func();
+       func.num = 1;
+       Func func2 = new Func();
+       func2.num=2;
+       System.out.println("交换前："+func.num+" "+func2.num);
+       Swap(func,func2);
+        System.out.println("交换后："+func.num+" "+func2.num);
+    }
+}
