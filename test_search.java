d33ffab0f2684c86902d8fd9cde2c01b9cd3161e
zbossz <77266547+zbossz@users.noreply.github.com>
2022-03-02 23:13:34 +0800

    jdbc

---
+++ test_search.java
@@ -0,0 +1,31 @@
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class test_search {
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = new MysqlDataSource();
+        ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/z1?" +
+                "characterEncoding=utf-8&useSSL=true");
+        ((MysqlDataSource)dataSource).setUser("");
+        ((MysqlDataSource)dataSource).setPassword("");
+        Connection connection = dataSource.getConnection();
+
+        String sql = "select * from student";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            System.out.println(id+"\t"+name);
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
